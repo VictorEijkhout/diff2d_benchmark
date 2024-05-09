39a6930034b023d0946ca0ba0dc60eea6407b39d
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-05-09 17:39:18 -0500

    bunch or srun tests

--- code/diff2d/span/compare_intel.sh
+++ code/diff2d/span/compare_intel.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 for q in skx icx spr ; do
+    echo && echo "================ submit to: $q" && echo
     export TACC_SYSTEM=$q
-    srun -p $q -t 0:10:0 \
-	 make run_scaling NSIZE=40000
+    srun -p $q -t 0:30:0 -N 1 -n 1 -A A-ccsc \
+	 make run_scaling NSIZE=20000 GITADD=1
 done
