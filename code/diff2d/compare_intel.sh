b8dda02ca777cdbcbc92f583b14fbb5036258dde
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-06-14 10:42:22 -0500

    almost a general queue/code script

--- code/diff2d/compare_intel.sh
+++ code/diff2d/compare_intel.sh
@@ -6,17 +6,26 @@
 ################################################################
 
 function usage () {
-    echo "Usage: $0 [ -c c1,c2,c3 (default: ${codes}) ] }"
-    echo "    [ -g (git add ) ] [ -s (srun; default: ${srun}) ]"
-    echo "    [ -q q1,q2,q3 (default: ${queues}) ] [ -t (trace) ] "
+    echo "Usage: $0 "
+    echo "    [ -c c1,c2,c3 (default: ${codes}) ] }"
+    echo "    [ -q q1,q2,q3 (default: ${queues}) ]"
+    echo "    [ -n 12345 (default: ${size} ]"
+    echo "    [ -g (git add ) ] [ -s (srun; default: ${srun}) ] [ -t (trace) ]"
 }
 
+codes=oned
+queues=skx,icx,spr,grx
+cpus_skx=48
+cpus_icx=80
+cpus_spr=112
+cpus_grx=240
+logfile=intel.log
+
+nsize=25000
 gitadd=0
 srun=1
 trace=
-codes=oned
-queues=skx,icx,spr,systest-grx
-logfile=intel.log
+
 while [ $# -gt 0 ] ; do
     if [ $1 = "-h" ] ; then 
 	usage && exit 1
@@ -24,6 +33,8 @@ while [ $# -gt 0 ] ; do
 	shift && codes=$1 && shift
     elif [ $1 = "-g" ] ; then 
 	gitadd=1 && shift
+    elif [ $1 = "-n" ] ; then
+	shift && nsize=$1 && shift
     elif [ $1 = "-q" ] ; then
 	shift && queues=$1 && shift
     elif [ $1 = "-s" ] ; then 
@@ -37,9 +48,10 @@ done
 
 for code in $( echo $codes | tr ',' ' ' ) ; do
     for queue in $( echo $queues | tr ',' ' ' ) ; do
-	cpus=112
+	eval cpus=\${cpus_${queue}}
+	if [ ${queue} = "grx" ] ; then queue=systest-grx; fi
 	mask=$( python3 ../utils/maskgen.py ${cpus} 1 )
-	echo && echo "================ submit to queue=$queue, proc code=$code" && echo
+	echo && echo "================ submit to queue=$queue, proc code=$code cores=$cpus" && echo
 	( cd ${code} \
 	      && if [ "${srun}" = "1" ] ; then \
 	      cmdline="srun -p $queue -t 0:30:0 -N 1 -n 1 -A A-ccsc \
@@ -48,7 +60,7 @@ for code in $( echo $codes | tr ',' ' ' ) ; do
 	      cmdline="" \
 	      ; fi \
 	      && cmdline="$cmdline \
-	    make run_scaling NSIZE=25000 GITADD=${gitadd} \
+	    make run_scaling NSIZE=${nsize} GITADD=${gitadd} \
 	      TACC_SYSTEM=$queue \
 	      THREADSYSTEM=$( \
 	        if [ \"${code}\" = \"sycl\" ] ; then echo dpcpp ; else echo omp ; fi ) \
