07ba64f1ab9836be017e152889b54442a7d5bd08
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-07-01 10:20:08 -0500

    diy works

--- code/diff2d/lib/diy2d.hpp
+++ code/diff2d/lib/diy2d.hpp
@@ -39,32 +39,43 @@ namespace linalg {
     /*
      * Iterator class
      */
-    class twod_iterator {
+    class cartesian_product_diy {
     private: 
       std::int64_t m, n; int b;
-      std::int64_t i{0}, j{0}; 
     public:
-      twod_iterator( std::int64_t m, std::int64_t n,int b ) 
-	: m(m),n(n),b(b) {};
-      auto& begin() { return *this; };
-      auto& end() { i=m; j=0; return *this; };
-      bool operator!=( const twod_iterator& other ) const {
-	return this->j!=other.j or this->i!=other.i; };
-      auto operator*() const {
-	return std::make_pair( (i+b), (j+b) ); };
-      auto& operator++(  ) {
-	j++; i+= (j/m); j = j%m; return *this; };
-      auto operator+( std::int64_t dist ) const {
-	auto displaced(*this);
-	auto lin = ( i*m+j ) + dist;
-	displaced.i = lin/m; displaced.j = lin%m; 
-	return displaced;
+      class cartesian_iterator {
+      private:
+        std::int64_t m, n; int b; /* global domain description */
+        std::int64_t i{0}, j{0};  /* local iteration point */
+      public:
+        cartesian_iterator\
+            ( std::int64_t m, std::int64_t n, int b, std::int64_t i, std::int64_t j )
+              : m(m),n(n),b(b),i(i),j(j) {};
+        bool operator==( const cartesian_iterator& other ) const {
+          return this->j==other.j and this->i==other.i; };
+        auto operator*() const {
+          // std::cout << std::format("deref {},{}\n",i,j);
+          return std::make_pair( (i+b), (j+b) ); };
+        auto& operator++(  ) {
+          j++; i+= (j/m); j = j%m; return *this; };
+        auto operator+( std::int64_t dist ) const {
+          auto displaced(*this);
+          auto lin = ( i*m+j ) + dist;
+          displaced.i = lin/m; displaced.j = lin%m; 
+          return displaced;
+        };
+        std::int64_t operator-( const cartesian_iterator& other ) const {
+          return (i-other.i)*m + (j-other.j); }
       };
-      std::int64_t operator-( const twod_iterator& other ) const {
-	return (i-other.i)*m + (j-other.j); }
+      cartesian_product_diy( std::int64_t m, std::int64_t n,int b ) 
+        : m(m),n(n),b(b) {};
+      auto begin() {
+        return cartesian_iterator(m,n,b, /* start */ 0,0); };
+      auto end()   {
+        return cartesian_iterator(m,n,b, /* end   */ m,0); };
     };
-    auto inner_diy() {
-      return twod_iterator(this->m(),this->n(),this->border());
+    auto inner_diy() { 
+      return cartesian_product_diy(this->m(),this->n(),this->border());
     };
   };
 };
